3fbf6a572ccc6c0199095523a71c6aaead6a4334
Aryan-986 <aryankarki984@gmail.com>
2025-02-05 02:17:18 +0545

    making server ready for starting the projects

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ import express from 'express'
 
 const app = express();
 
+//lets create routes
+app.get("/", (req, res) => {      //route in an Express.js server that listens for GET requests at the root URL ("/"). When a request is made to this URL, it responds with the text "server is ready". It's typically used to check if the server is running correctly.
+    res.send ("server is ready")
+})
+
 app.listen(5000, () => {
-    comsole.log("Server is running at http://localhost:5000")
+    console.log("Server is running at http://localhost:5000")
 });
 
 
