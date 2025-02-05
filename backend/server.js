51406a62b2721bcac713066f8917aec2a3c480a0
Aryan-986 <aryankarki984@gmail.com>
2025-02-05 14:56:04 +0545

    fix: properly mount auth routes with app.use() and also completed the required backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 import express from 'express'
+import authRoutes from './routes/auth.route.js'
 
 const app = express();
 
-//lets create routes
-app.get("/", (req, res) => {      //route in an Express.js server that listens for GET requests at the root URL ("/"). When a request is made to this URL, it responds with the text "server is ready". It's typically used to check if the server is running correctly.
-    res.send ("server is ready")
-})
+app.use("/api/v1/auth", authRoutes);
+
 
 app.listen(5000, () => {
     console.log("Server is running at http://localhost:5000")
